d226e85586caabf986d11227469afffff37ded0e
Kevin J Wang <kevinwangqi2@gmail.com>
2026-04-25 21:59:27 -0700

    feat: experiment with new section titles

--- schedule.js
+++ schedule.js
@@ -37,7 +37,7 @@ const SECTION_3 = {
 
 const SECTION_4 = {
     emoji: "🐼",
-    title: "Types You Can Create!",
+    title: "Types You Can Create?!",
     date: new Date(2026, 5 - 1, 1),
     slides: "https://docs.google.com/presentation/d/1iNgw5X3x7wVKUr1NLeLs_uUeoyq6MP-7zA0_kMbpfKg/edit?usp=sharing",
     handout: "https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1266/section/section4/",
@@ -47,7 +47,7 @@ const SECTION_4 = {
 
 const SECTION_5 = {
     emoji: "☝️",
-    title: "Pointer Mayhem, or That One Spiderman Meme",
+    title: "\"Thank you for the memories!\"",
     date: new Date(2026, 5 - 1, 8),
     slides: "https://docs.google.com/presentation/d/1va5uI8gVogtk1nMlRTHuTcq4G9xraQoQZjTWHV75ZiI/edit?usp=sharing",
     handout: "https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1266/section/section5/",
